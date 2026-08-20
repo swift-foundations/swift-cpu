0ebf384050afcbf15780aaf02226d626e85996e9
Coen ten Thije Boonkkamp <coen@coenttb.com>
2026-08-20 10:44:11 +0200

    Format and require Swift 6.4

--- Package.swift
+++ Package.swift
@@ -1,15 +1,15 @@
-// swift-tools-version: 6.3.3
+// swift-tools-version: 6.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-cpu",
     platforms: [
-        .macOS("27"),
-        .iOS("27"),
-        .tvOS("27"),
-        .watchOS("27"),
-        .visionOS("27")
+        .macOS(.v27),
+        .iOS(.v27),
+        .tvOS(.v27),
+        .watchOS(.v27),
+        .visionOS(.v27),
     ],
     products: [
         .library(
@@ -18,7 +18,10 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/swift-primitives/swift-cpu-primitives.git", branch: "main"),
+        .package(
+            url: "https://github.com/swift-primitives/swift-cpu-primitives.git",
+            branch: "main"
+        ),
         .package(url: "https://github.com/swift-intel/swift-x86-standard.git", branch: "main"),
         .package(url: "https://github.com/swift-arm-ltd/swift-arm-standard.git", branch: "main"),
     ],
@@ -27,16 +30,24 @@ let package = Package(
             name: "CPU",
             dependencies: [
                 .product(name: "CPU Primitives", package: "swift-cpu-primitives"),
-                .product(name: "X86 Standard", package: "swift-x86-standard",
-                         condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS, .visionOS, .linux, .windows])),
-                .product(name: "ARM Standard", package: "swift-arm-standard",
-                         condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS, .visionOS, .linux])),
+                .product(
+                    name: "X86 Standard",
+                    package: "swift-x86-standard",
+                    condition: .when(platforms: [
+                        .macOS, .iOS, .tvOS, .watchOS, .visionOS, .linux, .windows,
+                    ])
+                ),
+                .product(
+                    name: "ARM Standard",
+                    package: "swift-arm-standard",
+                    condition: .when(platforms: [.macOS, .iOS, .tvOS, .watchOS, .visionOS, .linux])
+                ),
             ]
         ),
         .testTarget(
             name: "CPU Tests",
             dependencies: [
-                "CPU",
+                "CPU"
             ]
         ),
     ],
